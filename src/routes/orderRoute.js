b540a6c4a1399af30cd0e4cdd1289e6cfed269a9
unknown <2000caothehai@gmail.com>
2021-10-15 18:21:23 +0700

    redeploy

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,10 +1,25 @@
-var express=require('express')
-const { getOrder, addOrder, getOneOrder, paymentByCard } = require('../controllers/order')
-const { requireSignin, sendEmailCardPayment } = require('../middleware/middleware')
-const router=express.Router()
-router.get('/',requireSignin,getOrder)
-router.post('/add-order',requireSignin,addOrder)
-router.post('/get-one-order',requireSignin,getOneOrder)
+var express = require("express");
+const {
+  getOrder,
+  addOrder,
+  getOneOrder,
+  paymentByCard,
+} = require("../controllers/order");
+const {
+  requireSignin,
+  sendEmailCardPayment,
+} = require("../middleware/middleware");
+const router = express.Router();
+router.get("/", requireSignin, getOrder);
+router.post("/add-order", requireSignin, addOrder);
+router.post("/get-one-order", requireSignin, getOneOrder);
 // router.post('/paymentCard',requireSignin,paymentByCard,addOrder)
-router.post('/paymentCard',requireSignin,sendEmailCardPayment,paymentByCard,addOrder)
-module.exports=router
+router.post(
+  "/paymentCard",
+  requireSignin,
+  sendEmailCardPayment,
+  paymentByCard,
+  addOrder
+);
+
+module.exports = router;
